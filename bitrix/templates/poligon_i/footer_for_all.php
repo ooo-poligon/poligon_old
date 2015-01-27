647d4f058f5da6a95e1c76e2528d46e4bebd9a5b
Igor Klekotnev <webmaster-poligon@yandex.ru>
2015-01-27 12:59:02 +0300

    Work 27_01_2015

---
+++ bitrix/templates/poligon_i/footer_for_all.php
@@ -0,0 +1,43 @@
+<footer class="footer_container">
+		<ul id="footer_list">
+
+			<li><a href="/content/about/">О компании</a></li>
+			
+			<li><a href="/catalog/">Каталог</a></li>
+	
+			<li><a href="#" id="trigger_1">Контактная информация</a></li>
+	
+			<li><a href="/map.php">Карта сайта</a></li>
+	
+			<li><a href="/content/links/">Обмен ссылками</a></li>
+		</ul>
+		<p>© 2014 ООО "ПОЛИГОН"<br>Все права защищены</p>
+		<div align="center">
+			<a href="http://yandex.ru/cy?base=0&amp;host=poligon.info"><img src="http://www.yandex.ru/cycounter?poligon.info" width="88" height="31" alt="Индекс цитирования" border="0" /></a>
+
+			<!-- Yandex.Metrika informer -->
+			<a href="https://metrika.yandex.ru/stat/?id=9739159&amp;from=informer"
+			target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/9739159/3_0_FFFFFFFF_E6E6FAFF_0_pageviews"
+			style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:9739159,lang:'ru'});return false}catch(e){}"/></a>
+			<!-- /Yandex.Metrika informer -->
+
+			<!-- begin of Top100 code -->
+			<script id="top100Counter" type="text/javascript"
+			src="http://counter.rambler.ru/top100.jcn?3056713"></script>
+			<noscript>
+			<a href="http://top100.rambler.ru/navi/3056713/">
+			<img src="http://counter.rambler.ru/top100.cnt?3056713"  height="31" width="88"  alt="Rambler's Top100"
+			border="0" />
+			</a></noscript>
+			<!-- end of Top100 code -->
+
+			<!-- Rating@Mail.ru logo -->
+			<a href="http://top.mail.ru/jump?from=299343">
+			<img src="//top-fwz1.mail.ru/counter?id=299343;t=441;l=1" 
+			style="border:0;" height="31" width="88" alt="Рейтинг@Mail.ru" /></a>
+			<!-- //Rating@Mail.ru logo -->
+		</div>
+</footer>
+<!-- RedHelper -->
+<!--<script id="rhlpscrtg" type="text/javascript" charset="utf-8" async="async" src="https://web.redhelper.ru/service/main.js?c=poligon"></script>-->
+<!--/Redhelper -->
--- bitrix/templates/poligon_ibx_tele/footer.php
+++ bitrix/templates/poligon_ibx_tele/footer.php
@@ -6,24 +6,7 @@
 </div>
 <br><br>
 <!--#################################################################################################################-->
-<footer class="footer_container">
-		<ul id="footer_list">
-
-			<li><a href="/content/about/">О компании</a></li>
-			
-			<li><a href="/catalog/">Каталог</a></li>
-			
-			<li><a href="#" id="trigger_1">Контактная информация</a></li>
-	
-			<li><a href="/map.php">Карта сайта</a></li>
-	
-			<li><a href="/content/links/">Обмен ссылками</a></li>
-		</ul>
-		<p>© 2014 ООО "ПОЛИГОН"<br>Все права защищены</p>
-</footer>
-<!-- RedHelper -->
-<script id="rhlpscrtg" type="text/javascript" charset="utf-8" async="async" src="https://web.redhelper.ru/service/main.js?c=poligon"></script>
-<!--/Redhelper -->
+<?php $APPLICATION->IncludeFile("/bitrix/templates/poligon_i/footer_for_all.php"); ?>
 <!--#################################################################################################################-->
 </body>
 </html>
